6ffb3e30093b15fae5408e71f599d7b6876a17b6
Yuko Sugiyama <sugi_yuko@hotmail.com>
2015-06-10 23:58:28 +1000

    Add home page

--- js/base.js
+++ js/base.js
@@ -1,11 +1,21 @@
 $(document).ready(function() {
     $('.menu a').click(function(e) {
         $(this).toggleClass('active');
-        // $('.toggle-nav').slideToggle(200);
-        $('.menu ul').toggleClass('active');
-        $('#panel-btn-icon').toggleClass('close');
-        return false;
-
+        $('.menu ul').slideToggle(300, function(){
+          $('.menu ul').toggleClass('active');
+          $('#panel-btn-icon').toggleClass('close');
+          return false;
+        })
         e.preventDefault();
     });
+
+    $('.read_more').click(function(){
+      $('.more_message').slideToggle(200);
+      return false;
+    });
+
+    $('.mouse_show').hover(function(){
+        $(this).closest('.grid_item').find('.line').slideToggle();
+    });
+
 });
