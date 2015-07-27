d509a667e4d5c6e963c18666503285ea584088b6
Yuko Sugiyama <sugi_yuko@hotmail.com>
2015-07-27 15:20:42 +1000

    Home contents: change JS to CSS

--- js/base.js
+++ js/base.js
@@ -26,27 +26,27 @@ $(document).ready(function() {
     });
 
 // Change from slide toggle to visible function
-    $('.grid_item.inside').hover(function(){
+    // $('.grid_item.inside').hover(function(){
 
-      var $this = $(this);
+    //   var $this = $(this);
 
-        var $that = $this.find('li');
-        $that.data('clr', $that.css('color')).css('color', '#656765');
-      },
-      function(){
-        var $this = $(this);
+    //     var $that = $this.find('li');
+    //     $that.data('clr', $that.css('color')).css('color', '#656765');
+    //   },
+    //   function(){
+    //     var $this = $(this);
 
-        var $that = $this.find('li');
-        $that.css('color', $that.data('clr'));
-      }
-    );
+    //     var $that = $this.find('li');
+    //     $that.css('color', $that.data('clr'));
+    //   }
+    // );
 
-// #656765
+// #656765 dark grey color
   // background image change its opaque
   // $('.grid_item').hover(function(){
   //   var $this = $(this);
   //   var $that = $this.find('.hover');
-  //   var $clickable = $this.find('.sb_behind');
+  //   var $clickable = $this.find('.behind');
   //   $that.data('opq', $that.css('opacity')).css('opacity', '0.1');
   //   $clickable.data('dis', $clickable.css('display')).css('display', 'inline-block');
 
@@ -54,7 +54,7 @@ $(document).ready(function() {
   // function(){
   //   var $this = $(this);
   //   var $that = $this.find('.hover');
-  //   var $clickable = $this.find('.sb_behind');
+  //   var $clickable = $this.find('.behind');
   //   $that.css('opacity', $that.data('opq'));
   //   $clickable.css('display', $clickable.data('dis'));
 
